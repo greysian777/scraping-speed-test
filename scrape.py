f54cc23f8bf3d574b0338cea0c17a80d82f1aaa5
svmihar <svmihar@gmail.com>
2020-05-31 00:48:45 +0700

    :books: results and stuf

--- scrape.py
+++ scrape.py
@@ -1,10 +1,12 @@
 import requests
+from tqdm import tqdm
 import time
-from typing import Set, List
+from typing import List
 from bs4 import BeautifulSoup
 import concurrent.futures
+import joblib
 
-LINKS = set(open("./test_links.txt").read().splitlines())
+LINKS = open("./ALL.txt").read().splitlines()[:int(1e4)]
 
 
 def get_paragraf(link: str) -> str:
@@ -14,25 +16,32 @@ def get_paragraf(link: str) -> str:
     return " ".join([p.text for p in reader.find_all("p") if "Baca juga" not in p.text])
 
 
-def run(links: Set) -> List[str]:
+def run(links: List) -> List[str]:
     return [get_paragraf(link) for link in links]
 
 
-def run_thread(links: Set) -> List[str]:
+def run_thread(links: List) -> List[str]:
     with concurrent.futures.ThreadPoolExecutor() as exe:
         results = exe.map(get_paragraf, links)
     return results
 
 
-def run_mp(links: Set) -> List[str]:
+def run_mp(links: List) -> List[str]:
     with concurrent.futures.ProcessPoolExecutor() as mp:
         result = mp.map(get_paragraf, links)
     return result
 
 
 if __name__ == "__main__":
-    start = time.time()
-    run_mp(LINKS)
-    end = time.time() - start
-    with open("timelog", "a") as f:
-        f.writelines(f"python run {str(time.time())}: {end}s\n")
+    scrape_sizes = range(200, len(LINKS), 500)
+    logs = []
+    for size in tqdm(scrape_sizes):
+        links = LINKS[:size]
+        start = time.time()
+        run_mp(links)
+        end = time.time() - start
+        logs.append({
+            'size': size,
+            'time': end
+        })
+    joblib.dump(logs, 'timelog_python.pkl')
