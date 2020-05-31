b939192190385b0480590f8c386c00279a735d06
svmihar <svmihar@gmail.com>
2020-06-01 01:25:23 +0700

    :fire: finished with python's analysis

--- scrape.py
+++ scrape.py
@@ -1,18 +1,18 @@
-import requests
-from tqdm import tqdm
-import time
+from requests import get
+#from tqdm import tqdm
+#import time
 from typing import List
 from bs4 import BeautifulSoup
-import concurrent.futures
-import joblib
+#import concurrent.futures
+#import joblib
 
-LINKS = open("./ALL.txt").read().splitlines()[:int(1e4)]
+LINKS = open("./ALL.txt").read().splitlines()[: int(1e4)]
 
 
 def get_paragraf(link: str) -> str:
-    r = requests.get(link)
+    r = get(link)
     soup = BeautifulSoup(r.content, "lxml")
-    reader = soup.find("div", {"class": "read__content"})
+    reader = soup.find("div",class_="read__content")
     return " ".join([p.text for p in reader.find_all("p") if "Baca juga" not in p.text])
 
 
@@ -33,15 +33,28 @@ def run_mp(links: List) -> List[str]:
 
 
 if __name__ == "__main__":
+    #start = time.time()
+    print('getting links')
+    # hasil = run_mp(LINKS[:200])
+    hasil = run(LINKS[:200])
+    print('finished scraping')
+    with open('hasil.txt', 'w') as f: 
+        for a in hasil: 
+            f.writelines(f'{a}\n')
+    #end = time.time() - start
+    #with open("timelog", "a") as f:
+        # f.writlines(f"python run: {end}s")
+    """
     scrape_sizes = range(200, len(LINKS), 500)
     logs = []
     for size in tqdm(scrape_sizes):
         links = LINKS[:size]
-        start = time.time()
+         start = time.time()
         run_mp(links)
-        end = time.time() - start
+        end = time.time()-start   
         logs.append({
             'size': size,
             'time': end
         })
     joblib.dump(logs, 'timelog_python.pkl')
+    """
